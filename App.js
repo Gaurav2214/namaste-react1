f4829776205a37c4ab97a87436baa3a979b02e9e
Gaurav Tripathi <ramantripathi22@gmail.com>
2023-07-18 15:39:44 +0530

    changes

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement
     ("h1",
     {"id":"head1", "datanode": "node1"},
@@ -5,11 +7,18 @@ const heading = React.createElement
     );
 
 const parent = [React.createElement(
-    "div", {id:"parent"}, React.createElement("div", {id:"child"}, 
-    [React.createElement("h1", {id:"h1"}, "I am H1 tag."), React.createElement("h2", {id:"h3"}, "I am H2 tag.")])
-), React.createElement(
+    "div", 
+    {id:"parent"}, React.createElement("div", {id:"child"}, 
+    [ 
+        React.createElement("h1", {id:"h1"}, "I am Namaste React."), 
+        React.createElement("h2", {id:"h3"}, "Parcel is a beast.")
+    ])
+    ), React.createElement(
     "div", {id:"parent2"}, React.createElement("div", {id:"child2"}, 
-    [React.createElement("h1", {id:"h2"}, "I am H1 tag."), React.createElement("h2", {id:"h4"}, "I am H2 tag.")])
+    [
+        React.createElement("h1", {id:"h2"}, "I am H1 tag."), 
+        React.createElement("h2", {id:"h4"}, "I am H2 tag.")
+    ])
 )];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ README.md
@@ -0,0 +1,20 @@
+#Namaste React 
+
+#Parcel
+- Dev Build
+- Local Server
+- HMR Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Diffrential Bundling - support older browsers
+- Diagontic
+- Error handling
+- HTTPs
+- Tree Shaking - remove unused code
+- https://parceljs.org/
